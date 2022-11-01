8f109d0279ccab411bdec0d84efd83cf28de13d6
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-01 09:46:54 +0500

    tag increment

--- Sources/PrepDataTypes/Models/User.swift
+++ Sources/PrepDataTypes/Models/User.swift
@@ -9,17 +9,15 @@ public struct User: Identifiable, Hashable {
     public var explicitVolumeUnits: UserExplicitVolumeUnits
     public var bodyMeasurements: BodyMeasurements
     
-    public var syncStatus: SyncStatus
     public var updatedAt: Date
     
-    public init(id: UUID, cloudKitId: String?, preferredEnergyUnit: EnergyUnit, prefersMetricUnits: Bool, explicitVolumeUnits: UserExplicitVolumeUnits, bodyMeasurements: BodyMeasurements, syncStatus: SyncStatus, updatedAt: Date) {
+    public init(id: UUID, cloudKitId: String?, preferredEnergyUnit: EnergyUnit, prefersMetricUnits: Bool, explicitVolumeUnits: UserExplicitVolumeUnits, bodyMeasurements: BodyMeasurements, updatedAt: Date) {
         self.id = id
         self.cloudKitId = cloudKitId
         self.preferredEnergyUnit = preferredEnergyUnit
         self.prefersMetricUnits = prefersMetricUnits
         self.explicitVolumeUnits = explicitVolumeUnits
         self.bodyMeasurements = bodyMeasurements
-        self.syncStatus = syncStatus
         self.updatedAt = updatedAt
     }
 }

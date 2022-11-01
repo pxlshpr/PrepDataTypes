8740307529cfab9b051fffeb77b4b8cc98b12a43
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-01 18:56:20 +0500

    tag increment

---
+++ Sources/PrepDataTypes/Extensions/Notification.Name.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+public extension Notification.Name {
+    static var dayPagerWillChangeDate: Notification.Name { return .init("dayPagerWillChangeDate") }
+    static var weekPagerWillChangeDate: Notification.Name { return .init("weekPagerWillChangeDate") }
+    static var weekPagerDidChangeDate: Notification.Name { return .init("weekPagerDidChangeDate") }
+    
+    static var diarySummaryDetentChangedToMedium: Notification.Name { return .init("diarySummaryDetentChangedToMedium") }
+
+    /// To move to PrepDataTypes
+    static var dateDidChange: Notification.Name { return .init("dateDidChange") }
+    static var diaryWillChangeDate: Notification.Name { return .init("diaryWillChangeDate") }
+    static var didPickDateOnDayView: Notification.Name { return .init("didPickDateOnDayView") }
+    static var didAddFoodItemToMeal: Notification.Name { return .init("didAddFoodItemToMeal") }
+    static var summaryViewTypeChanged: Notification.Name { return .init("summaryViewTypeChanged") }
+    
+    static var foodItemCompletionDidChange: Notification.Name { return .init("foodItemCompletionDidChange") }
+    static var didPickDateForMealDuplication: Notification.Name { return .init("didPickDateForMealDuplication") }
+    
+    static var didTapAddMealButton: Notification.Name { return .init("didTapAddMealButton") }
+    
+    /// Backend related
+    static var didUpdateUser: Notification.Name { return .init("didUpdateUser") }
+    static var didAddMeal: Notification.Name { return .init("didAddMeal") }
+}
+
+public extension Notification {
+    struct Keys {
+        static let foodItem = "foodItem"
+        static let date = "date"
+        static let summaryViewTypeRawValue = "summaryViewTypeRawValue"
+        static let sender = "sender"
+        static let url = "url"
+        static let meal = "meal"
+    }
+}

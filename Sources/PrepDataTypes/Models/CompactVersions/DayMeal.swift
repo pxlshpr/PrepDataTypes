2dd3d78b4f60cccc815572969c8bb495c4c2a087
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-03 05:58:56 +0500

    tag increment

---
+++ Sources/PrepDataTypes/Models/CompactVersions/DayMeal.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/// This is a summarised version of a Meal to be placed inside a `Day` object
+/// It is stripped off the relation back to the `Day` so as to not clear a cyclical recursion error,
+/// in addition to not having the `syncStatus`, `updatedAt`, `deletedAt` metadata.
+/// This is only to be used as a child of a `Meal` to be used in the UI.
+public struct DayMeal: Identifiable, Hashable, Codable {
+    public let id: UUID
+    public var name: String
+    public var time: Double
+    public var markedAsEatenAt: Double?
+    public var foodItems: [FoodItem]
+
+    public init(id: UUID, name: String, time: Double, markedAsEatenAt: Double? = nil, foodItems: [FoodItem]) {
+        self.id = id
+        self.name = name
+        self.time = time
+        self.markedAsEatenAt = markedAsEatenAt
+        self.foodItems = foodItems
+    }
+}

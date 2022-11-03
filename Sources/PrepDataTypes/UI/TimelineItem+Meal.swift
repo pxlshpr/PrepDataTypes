7efd0c8ecd9f47a7046ce93ee7612e2ff970e018
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-03 06:08:22 +0500

    tag increment

--- Sources/PrepDataTypes/UI/TimelineItem+Meal.swift
+++ Sources/PrepDataTypes/UI/TimelineItem+Meal.swift
@@ -1,9 +1,19 @@
 import Foundation
 
-
-
 public extension TimelineItem {
     convenience init(meal: Meal) {
+        self.init(
+            id: meal.id.uuidString,
+            name: meal.name,
+            date: Date(timeIntervalSince1970: meal.time),
+            emojis: [],
+            type: .meal
+        )
+    }
+}
+
+public extension TimelineItem {
+    convenience init(dayMeal meal: DayMeal) {
         self.init(
             id: meal.id.uuidString,
             name: meal.name,

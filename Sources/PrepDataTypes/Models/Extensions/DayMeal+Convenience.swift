6856d2377850d7efd62b850237444cf7191af8d3
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-04 20:14:43 +0500

    tag increment

--- Sources/PrepDataTypes/Models/Extensions/DayMeal+Convenience.swift
+++ Sources/PrepDataTypes/Models/Extensions/DayMeal+Convenience.swift
@@ -21,9 +21,11 @@ public extension DayMeal {
         foodItems.reduce(0) { $0 + $1.energyAmount }
     }
     
+#if os(iOS) || os(macOS)
     var timeString: String {
         Date(timeIntervalSince1970: time).formatted(date: .omitted, time: .shortened).lowercased()
     }
+#endif
     
 //    var timeDate: Date {
 //        get { Date(timeIntervalSince1970: TimeInterval(time)) }

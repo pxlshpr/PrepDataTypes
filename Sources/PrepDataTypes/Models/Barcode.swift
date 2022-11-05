ef3c94578af403a65e60f6e21a27a2447175cd49
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-05 17:30:16 +0500

    tag increment

--- Sources/PrepDataTypes/Models/Barcode.swift
+++ Sources/PrepDataTypes/Models/Barcode.swift
@@ -2,13 +2,13 @@ import Foundation
 
 public struct Barcode: Identifiable, Hashable, Codable {
     public let id: UUID
-    public let food: Food
+//    public let food: Food
     public let payload: String
     public let symbology: BarcodeSymbology
     
-    public init(id: UUID, food: Food, payload: String, symbology: BarcodeSymbology) {
+    public init(id: UUID, payload: String, symbology: BarcodeSymbology) {
         self.id = id
-        self.food = food
+//        self.food = food
         self.payload = payload
         self.symbology = symbology
     }

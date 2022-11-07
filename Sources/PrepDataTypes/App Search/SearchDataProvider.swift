edfff8834c1697075f22c36390cb7243bba9f4e3
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-07 13:23:36 +0500

    tag increment

--- Sources/PrepDataTypes/App Search/SearchDataProvider.swift
+++ Sources/PrepDataTypes/App Search/SearchDataProvider.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public protocol SearchDataProvider {
     var recentFoods: [Food] { get }
-    func getFoods(scope: SearchScope, searchText: String, page: Int) async throws -> [Food]
+    func getFoods(scope: SearchScope, searchText: String, page: Int) async throws -> (foods: [Food], haveMoreResults: Bool)
 }

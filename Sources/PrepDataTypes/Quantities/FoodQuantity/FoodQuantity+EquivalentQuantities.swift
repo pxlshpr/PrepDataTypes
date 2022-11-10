b622dd8fde10d31f614868149f1cce4a1bf26202
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-10 15:02:43 +0500

    tag increment

--- Sources/PrepDataTypes/Quantities/FoodQuantity/FoodQuantity+EquivalentQuantities.swift
+++ Sources/PrepDataTypes/Quantities/FoodQuantity/FoodQuantity+EquivalentQuantities.swift
@@ -2,7 +2,8 @@ import Foundation
 
 extension FoodQuantity {
     func equivalentQuantities(using: UserExplicitVolumeUnits = .defaultUnits) -> [FoodQuantity] {
-        let possibleUnits = food.possibleUnits(without: unit, using: .defaultUnits)
+//        let possibleUnits = food.possibleUnits(without: unit, using: .defaultUnits)
+        let possibleUnits = food.possibleUnits(using: .defaultUnits)
         let equivalentQuantities = possibleUnits.compactMap { convert(to: $0) }
         return equivalentQuantities
     }
--- Tests/PrepDataTypesTests/PossibleUnitsTests.swift
+++ Tests/PrepDataTypesTests/PossibleUnitsTests.swift
@@ -8,18 +8,16 @@ final class PossibleUnitsTests: XCTestCase {
         
         let food = Food(
             mockName: "Carrot",
-            serving: .init(1, .cupMetric, "chopped4"), /// 250 mL
+            serving: .init(1, .cupUSLegal, "chopped4"), /// 240 mL
             sizes: [
                 .init(0.5, .cupUSLegal, "chopped", .init(135, .g)), /// 240 mL
                 .init(2, "medium", .init(100, .g)),
             ]
         )
         
-        
-        
         let quantity = FoodQuantity(1, food)
         guard let choppedUS = food.quantitySize(for: "chopped4") else { XCTFail(); return }
-        let choppedMetricUnit = FoodQuantity.Unit.size(choppedUS, .cupUSLegal)
+        let choppedMetricUnit = FoodQuantity.Unit.size(choppedUS, .cupMetric)
         let value = quantity.convert(to: choppedMetricUnit)
         
 //        let possibleUnits = quantity.food.possibleUnits(without: quantity.unit, using: .defaultUnits)

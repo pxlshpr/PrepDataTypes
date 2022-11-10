b0ddd29d0bbfeac93bcc5229852963c59544b4c3
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-10 11:14:39 +0500

    tag increment

--- Sources/PrepDataTypes/Models/Extensions/Food+PossibleUnits.swift
+++ Sources/PrepDataTypes/Models/Extensions/Food+PossibleUnits.swift
@@ -6,7 +6,14 @@ public extension Food {
         without unit: FoodQuantity.Unit,
         using userVolumeUnits: UserExplicitVolumeUnits) -> [FoodQuantity.Unit]
     {
-        possibleUnits(using: userVolumeUnits).filter { $0 != unit }
+        possibleUnits(using: userVolumeUnits).filter {
+            /// If the units are both sizes—compare the sizes alone to exclude any potential different volume prefixes
+            if let possibleSize = $0.size, let size = unit.size {
+                return possibleSize.id == size.id
+            } else {
+                return $0 != unit
+            }
+        }
     }
     
     func possibleUnits(using userVolumeUnits: UserExplicitVolumeUnits) -> [FoodQuantity.Unit] {
@@ -29,5 +36,15 @@ public extension Food {
             units.append(.volume(userVolumeUnits.teaspoon))
         }
         return units
+    }
+}
+
+extension FoodQuantity.Size {
+    public var id: String {
+        if let volumePrefixExplicitUnit {
+            return "\(name)\(volumePrefixExplicitUnit.volumeUnit.rawValue)"
+        } else {
+            return name
+        }
     }
 }

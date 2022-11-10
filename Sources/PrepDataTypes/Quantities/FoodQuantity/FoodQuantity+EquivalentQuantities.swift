ef70433c94079bf6dc170f2a8a4d877f26f4c6fb
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-10 15:13:16 +0500

    tag increment

--- Sources/PrepDataTypes/Quantities/FoodQuantity/FoodQuantity+EquivalentQuantities.swift
+++ Sources/PrepDataTypes/Quantities/FoodQuantity/FoodQuantity+EquivalentQuantities.swift
@@ -1,9 +1,8 @@
 import Foundation
 
-extension FoodQuantity {
+public extension FoodQuantity {
     func equivalentQuantities(using: UserExplicitVolumeUnits = .defaultUnits) -> [FoodQuantity] {
-//        let possibleUnits = food.possibleUnits(without: unit, using: .defaultUnits)
-        let possibleUnits = food.possibleUnits(using: .defaultUnits)
+        let possibleUnits = food.possibleUnits(without: unit, using: .defaultUnits)
         let equivalentQuantities = possibleUnits.compactMap { convert(to: $0) }
         return equivalentQuantities
     }

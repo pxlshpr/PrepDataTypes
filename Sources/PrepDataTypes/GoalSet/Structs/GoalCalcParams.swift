c8a4f554acedd310e8eca70e5496fbb49e68ce9c
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-24 13:36:00 +0500

    tag increment

--- Sources/PrepDataTypes/GoalSet/Structs/GoalCalcParams.swift
+++ Sources/PrepDataTypes/GoalSet/Structs/GoalCalcParams.swift
@@ -4,4 +4,10 @@ public struct GoalCalcParams {
     public let userUnits: UserUnits
     public let bodyProfile: BodyProfile?
     public let energyGoal: Goal?
+    
+    public init(userUnits: UserUnits, bodyProfile: BodyProfile?, energyGoal: Goal?) {
+        self.userUnits = userUnits
+        self.bodyProfile = bodyProfile
+        self.energyGoal = energyGoal
+    }
 }

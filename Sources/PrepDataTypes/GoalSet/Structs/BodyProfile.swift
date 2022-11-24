d6089e2580ede14a4a47ccb8609657b8357f7250
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-24 13:36:39 +0500

    tag increment

--- Sources/PrepDataTypes/GoalSet/Structs/BodyProfile.swift
+++ Sources/PrepDataTypes/GoalSet/Structs/BodyProfile.swift
@@ -43,6 +43,42 @@ public struct BodyProfile: Hashable, Codable {
     public var dobMonth: Int?
     public var dobYear: Int?
     public var ageSource: MeasurementSource?
+    
+    public init(energyUnit: EnergyUnit, weightUnit: WeightUnit, heightUnit: HeightUnit, restingEnergy: Double? = nil, restingEnergySource: RestingEnergySource? = nil, restingEnergyFormula: RestingEnergyFormula? = nil, restingEnergyPeriod: HealthPeriodOption? = nil, restingEnergyIntervalValue: Int? = nil, restingEnergyInterval: HealthAppInterval? = nil, activeEnergy: Double? = nil, activeEnergySource: ActiveEnergySource? = nil, activeEnergyActivityLevel: ActivityLevel? = nil, activeEnergyPeriod: HealthPeriodOption? = nil, activeEnergyIntervalValue: Int? = nil, activeEnergyInterval: HealthAppInterval? = nil, fatPercentage: Double? = nil, lbm: Double? = nil, lbmSource: LeanBodyMassSource? = nil, lbmFormula: LeanBodyMassFormula? = nil, lbmDate: Date? = nil, weight: Double? = nil, weightSource: MeasurementSource? = nil, weightDate: Date? = nil, height: Double? = nil, heightSource: MeasurementSource? = nil, heightDate: Date? = nil, sexIsFemale: Bool? = nil, sexSource: MeasurementSource? = nil, age: Int? = nil, dobDay: Int? = nil, dobMonth: Int? = nil, dobYear: Int? = nil, ageSource: MeasurementSource? = nil) {
+        self.energyUnit = energyUnit
+        self.weightUnit = weightUnit
+        self.heightUnit = heightUnit
+        self.restingEnergy = restingEnergy
+        self.restingEnergySource = restingEnergySource
+        self.restingEnergyFormula = restingEnergyFormula
+        self.restingEnergyPeriod = restingEnergyPeriod
+        self.restingEnergyIntervalValue = restingEnergyIntervalValue
+        self.restingEnergyInterval = restingEnergyInterval
+        self.activeEnergy = activeEnergy
+        self.activeEnergySource = activeEnergySource
+        self.activeEnergyActivityLevel = activeEnergyActivityLevel
+        self.activeEnergyPeriod = activeEnergyPeriod
+        self.activeEnergyIntervalValue = activeEnergyIntervalValue
+        self.activeEnergyInterval = activeEnergyInterval
+        self.fatPercentage = fatPercentage
+        self.lbm = lbm
+        self.lbmSource = lbmSource
+        self.lbmFormula = lbmFormula
+        self.lbmDate = lbmDate
+        self.weight = weight
+        self.weightSource = weightSource
+        self.weightDate = weightDate
+        self.height = height
+        self.heightSource = heightSource
+        self.heightDate = heightDate
+        self.sexIsFemale = sexIsFemale
+        self.sexSource = sexSource
+        self.age = age
+        self.dobDay = dobDay
+        self.dobMonth = dobMonth
+        self.dobYear = dobYear
+        self.ageSource = ageSource
+    }
 }
 
 public extension BodyProfile {

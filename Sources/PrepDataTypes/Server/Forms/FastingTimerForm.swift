b7a2156de2d073e3ccf35ab965f69fbd6d85440a
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-08 17:55:25 +0500

    tag increment

--- Sources/PrepDataTypes/Server/Forms/FastingTimerForm.swift
+++ Sources/PrepDataTypes/Server/Forms/FastingTimerForm.swift
@@ -1,11 +1,24 @@
 import Foundation
 
 public struct FastingTimerForm: Codable {
-    public let fastingTimerState: FastingTimerState?
+
+    public var lastMealAt: Double?
+    public var nextMealName: String?
+    public var nextMealAt: Double?
     public let userId: UUID
     
-    public init(fastingTimerState: FastingTimerState?, userId: UUID) {
-        self.fastingTimerState = fastingTimerState
+    public init(lastMealAt: Double?, nextMealName: String? = nil, nextMealAt: Double? = nil, userId: UUID) {
+        self.lastMealAt = lastMealAt
+        self.nextMealName = nextMealName
+        self.nextMealAt = nextMealAt
         self.userId = userId
     }
+    
+    public init(fastingTimerState state: FastingTimerState? = nil, userId: UUID) {
+        self.lastMealAt = state?.lastMealTime.timeIntervalSince1970
+        self.nextMealName = state?.nextMealName
+        self.nextMealAt = state?.nextMealTime?.timeIntervalSince1970
+        self.userId = userId
+    }
+
 }

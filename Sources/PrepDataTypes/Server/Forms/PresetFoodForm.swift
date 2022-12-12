abb1de5d81830796602dc666c6209914ad63b73d
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-12 13:01:46 +0500

    tag increment

--- Sources/PrepDataTypes/Server/Forms/PresetFoodForm.swift
+++ Sources/PrepDataTypes/Server/Forms/PresetFoodForm.swift
@@ -2,10 +2,12 @@ import Foundation
 
 public struct PresetFoodForm: Codable {
     public let id: String
+    public let dataset: FoodDataset
     public let food: Food
     
-    public init(id: String, food: Food) {
+    public init(id: String, dataset: FoodDataset, food: Food) {
         self.id = id
+        self.dataset = dataset
         self.food = food
     }
 }

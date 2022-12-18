9911ad60c3755617a1c49876301003ebd198b8c4
Ahmed Khalaf <pxlshpr@gmail.com>
2022-12-18 16:32:50 +0500

    bump

--- Sources/PrepDataTypes/UserDefaultsKeys.swift
+++ Sources/PrepDataTypes/UserDefaultsKeys.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 public struct UserDefaultsKeys {
+    public static let showingBadgesForFoods = "showingBadgesForFoods"
     public static let showingFoodDetails = "showingFoodDetails"
     public static let fastingTimerPushToken = "fastingTimerPushToken"
 }

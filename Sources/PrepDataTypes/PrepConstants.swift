c6ad1ebb14c565584f587fbe5ee42ef70df5ff70
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-14 23:33:08 +0500

    bump

--- Sources/PrepDataTypes/PrepConstants.swift
+++ Sources/PrepDataTypes/PrepConstants.swift
@@ -4,11 +4,17 @@ public struct PrepConstants {
     public static let bottomBarHeight: CGFloat = 95
     
     public static let numberOfRecents = 10
+    
+    /// The window size, which indicates how many more elements we have in each direction.
+    /// So a value of 1 would indicate a window of 3 elements, 3 would indicate 7, etc.
+    public static let slidingWindowSize = 7
+    public static let slidingWindowFullSize = (slidingWindowSize * 2) + 1
+
+    public static let dayIndices = Array(0..<slidingWindowFullSize)
+//    public static let dayIndices = [0, 1, 2, 3]
 }
 
-/// ** Conditional Print ** Prints if a certain condition is met
+/// Use this for print statements that we can disabled by commenting out one line.
 public func cprint(_ string: String) {
-    if true {
-        print(string)
-    }
+//    print(string)
 }

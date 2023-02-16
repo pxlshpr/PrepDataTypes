13ef7856229b27a7b7a4b6707e6489f992193591
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-16 15:12:25 +0500

    tag increment

--- Sources/PrepDataTypes/PrepConstants.swift
+++ Sources/PrepDataTypes/PrepConstants.swift
@@ -12,6 +12,13 @@ public struct PrepConstants {
 
     public static let dayIndices = Array(0..<slidingWindowFullSize)
 //    public static let dayIndices = [0, 1, 2, 3]
+    
+    
+    public struct DefaultPreferences {
+        public static let showingBadgesForFoods = true
+        public static let showingFoodDetails = false
+        public static let showingFoodEmojis = true
+    }
 }
 
 /// Use this for print statements that we can disabled by commenting out one line.

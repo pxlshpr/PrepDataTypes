bd7b6afeb116d7ee0b813900d10c1391e07a0cc7
Ahmed Khalaf <pxlshpr@gmail.com>
2023-03-05 16:06:12 +0500

    bump

--- Sources/PrepDataTypes/UserDefaultsKeys.swift
+++ Sources/PrepDataTypes/UserDefaultsKeys.swift
@@ -1,14 +1,6 @@
 import Foundation
 
 public struct UserDefaultsKeys {
-//    public static let showingBadgesForFoods = "showingBadgesForFoods"
-//    public static let showingFoodDetails = "showingFoodDetails"
-//    public static let showingFoodEmojis = "showingFoodEmojis"
-//    public static let showingIngredientDetails = "showingIngredientDetails"
-    public static let showingIngredientBadges = "showingIngredientBadges"
-    public static let showingIngredientEmojis = "showingIngredientEmojis"
-    
-    public static let lastSelectedMetricsTab = "lastSelectedMetricsTab"
     public static let chartPeriod = "chartPeriod"
     public static let showingMacrosForEnergyReport = "showingMacrosForEnergyReport"
     
